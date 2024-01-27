33eee68508abad622e531cb23de4f5701f8c32ed
devi varaprasad <vudidevivaraprasad143@gmail.com>
2024-01-27 17:05:22 +0530

    handeling the no image

--- src/services/GetCropImage.ts
+++ src/services/GetCropImage.ts
@@ -1,5 +1,7 @@
+import noImage from '../assets/no-image-placeholder.webp'
+
 const GetCropImage = (url:string) => {
-    if(!url) return url
+    if(!url) return noImage
     const index = url.indexOf('o/media')+ 'o/media'.length
     const new_url = url.slice(0,index) + "/crop/600/400" + url.slice(index)
     return new_url
