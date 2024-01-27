7342a41608a86b3b06c37b0dae586e03fc8716e6
devi varaprasad <vudidevivaraprasad143@gmail.com>
2024-01-27 19:58:55 +0530

    changed the font size of heading

--- src/components/GameHeading.tsx
+++ src/components/GameHeading.tsx
@@ -2,7 +2,7 @@ import { GameQuery } from "./Home"
 
 const Heading = ({gamequery}:{gamequery:GameQuery}) => {
     return(
-        <h1 className="mx-5 bold py-2 text-nowrap">{`${gamequery.genre?gamequery.genre.name+" ":""}Games`}</h1>
+        <h1 className="mx-5 bold py-2 text-nowrap display-6 fw-semibold">{`${gamequery.genre?gamequery.genre.name+" ":""}Games`}</h1>
     )
 }
 
